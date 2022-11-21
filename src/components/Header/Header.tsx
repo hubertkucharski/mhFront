d608c7fb2977e78c01c1e451ca8b5dd51ab4654e
hubertkucharski <hubertkucharski@gmail.com>
2022-11-21 11:37:06 +0100

    Logout confirmation alert

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,16 +6,23 @@ import {validateCurrUserAsync} from "../../redux/features/userSlice";
 import {useAppDispatch, useAppSelector} from "../../redux/app/hooks";
 import {Link} from "react-router-dom";
 import {apiUrl} from "../../config/apiUrl";
+import {Alert, Snackbar} from "@mui/material";
 
 export const Header = () => {
 
     const dispatch = useAppDispatch();
     const [isShown, setIsShown] = useState('AddForm');
+    const [openConfirmation, setOpenConfirmation] = useState(false);
     const userState = useAppSelector((state) => (state.user.user));
 
     useEffect(() => {
         dispatch(validateCurrUserAsync());
     }, []);
+
+    const handleCloseConfirmation = () => {
+        setOpenConfirmation(false);
+        window.location.reload();
+    };
 
     const logOut = async () => {
         const res = await fetch(`${apiUrl}/auth/logout`, {
@@ -24,13 +31,18 @@ export const Header = () => {
         const resJson = await res.json();
         if (resJson.message === 'Wylogowano') {
             await dispatch(validateCurrUserAsync());
-            window.location.reload();
+            setOpenConfirmation(true);
         }
     };
 
     return (
         <>
             <h1>Place where you can exchange board games</h1>
+            <Snackbar open={openConfirmation} autoHideDuration={3330} onClose={handleCloseConfirmation}>
+                <Alert onClose={handleCloseConfirmation} severity="success" sx={{ width: '100%' }}>
+                    You have been successfully logged out!
+                </Alert>
+            </Snackbar>
             <div className='container contact__container'>
                 <button type='button' className='btn btn-primary' onClick={
                     () => setIsShown('AddForm')}>
